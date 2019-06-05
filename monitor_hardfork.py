6328b6fbf0f2fef8e73930f269f2783202fcc223
Stanislav <s.kurganskiy@uptrader.io>
2019-06-05 08:32:37 +0300

    logging added

--- monitor_hardfork.py
+++ monitor_hardfork.py
@@ -1,40 +1,49 @@
 import collections
 import json
+import logging
 import statistics
 import time
 
 import websocket
 
+logging.basicConfig(
+    format="%(levelname)-8s %(asctime)s [%(filename)s:%(lineno)d] %(message)s",
+    level=logging.INFO,
+)
+
 WS_SUBSCRIBE_URL = "wss://ws.blockchain.info/inv"
 BLOCKS_FOR_MTP = 11
 
 while True:
+    hardfork_timestamp = input("Enter the UNIX timestamp for HardFork: \n")
+    logging.debug("The input value is '%s'", hardfork_timestamp)
     try:
-        hardfork_timestamp = int(input("Enter the UNIX timestamp for HardFork: "))
+        hardfork_timestamp = int(hardfork_timestamp)
     except ValueError:
-        print("Please, enter a valid UNIX timestamp. For example: 1559708603")
+        logging.error("An exception during handling '%s' input has happened", hardfork_timestamp)
+        logging.info("Please, enter a valid UNIX timestamp. For example: 1559708603")
         continue
 
     if hardfork_timestamp <= time.time():
-        print("This time is already in the past! Try again.")
+        logging.info("This time is already in the past! Try again.")
         continue
     else:
         break
 
-print("Establishing connection...")
+logging.info("Establishing connection...")
 ws = websocket.create_connection(WS_SUBSCRIBE_URL)
-print("Connection established!")
+logging.info("Connection established!")
 ws.send(json.dumps({"op": "blocks_sub"}))
-print("Sending data...")
+logging.info("Sending data...")
 
 blocks_timestamps = collections.deque(maxlen=BLOCKS_FOR_MTP)
 
 while True:
+    logging.info("Waiting for a block...")
     result = json.loads(ws.recv())
-    print(f"Received block with hash {result['x']['hash']}. Calculating...")
+    logging.info("Received block with hash %s. Calculating...", result['x']['hash'])
     blocks_timestamps.append(result['x']['time'])
     if len(blocks_timestamps) == BLOCKS_FOR_MTP and statistics.median(blocks_timestamps) >= hardfork_timestamp:
-        print("The HardFork has come! Beware...")
+        logging.info("The HardFork has come! Beware...")
         ws.close()
         break
-    print("Waiting for another block...")
