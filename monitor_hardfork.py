ef1c787708cef978cecfdac6ddcbbc9f4725505c
Stanislav <s.kurganskiy@uptrader.io>
2019-06-05 07:57:41 +0300

    constants added

--- monitor_hardfork.py
+++ monitor_hardfork.py
@@ -5,6 +5,9 @@ import time
 
 import websocket
 
+WS_SUBSCRIBE_URL = "wss://ws.blockchain.info/inv"
+BLOCKS_FOR_MTP = 11
+
 while True:
     try:
         hardfork_timestamp = int(input("Enter the UNIX timestamp for HardFork: "))
@@ -19,17 +22,18 @@ while True:
         break
 
 print("Establishing connection...")
-ws = websocket.create_connection("wss://ws.blockchain.info/inv")
+ws = websocket.create_connection(WS_SUBSCRIBE_URL)
 print("Connection established!")
 ws.send(json.dumps({"op": "blocks_sub"}))
 print("Sending data...")
-blocks_timestamps = collections.deque(maxlen=11)
+
+blocks_timestamps = collections.deque(maxlen=BLOCKS_FOR_MTP)
 
 while True:
     result = json.loads(ws.recv())
     print(f"Received block with hash {result['x']['hash']}. Calculating...")
     blocks_timestamps.append(result['x']['time'])
-    if len(blocks_timestamps) == 11 and statistics.median(blocks_timestamps) >= hardfork_timestamp:
+    if len(blocks_timestamps) == BLOCKS_FOR_MTP and statistics.median(blocks_timestamps) >= hardfork_timestamp:
         print("The HardFork has come! Beware...")
         ws.close()
         break
