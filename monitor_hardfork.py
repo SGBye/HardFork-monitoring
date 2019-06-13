90ee132050e3f8af914b234144b51f06d4bc6c37
SGBye <ctac1995@gmail.com>
2019-06-13 20:31:48 +0300

    fixed situation when some blocks could have already been part of 11-chain. Now it should be working under any circumstances.

--- monitor_hardfork.py
+++ monitor_hardfork.py
@@ -4,13 +4,15 @@ import logging
 import statistics
 import time
 
+import requests
 import websocket
 
 logging.basicConfig(
     format="%(levelname)-8s %(asctime)s [%(filename)s:%(lineno)d] %(message)s",
     level=logging.INFO,
 )
-
+time_now_ms = int(round(time.time() * 1000))
+GET_DAY_BLOCKS_URL = f"https://blockchain.info/blocks/{time_now_ms}?format=json"
 WS_SUBSCRIBE_URL = "wss://ws.blockchain.info/inv"
 BLOCKS_FOR_MTP = 11
 
@@ -30,20 +32,31 @@ while True:
     else:
         break
 
+# first get 11 latest blocks and check their median time not to miss anything
+r = requests.get(GET_DAY_BLOCKS_URL)
+
+blocks_timestamps = collections.deque((block['time'] for block in r.json()['blocks'][:11]), maxlen=BLOCKS_FOR_MTP)
+if statistics.median(blocks_timestamps) >= hardfork_timestamp:
+    logging.info("The HardFork has come! Beware...")
+    raise SystemExit("Thank you for using our program")
+
+logging.info("HardFork hasn't come yet.")
 logging.info("Establishing connection...")
 ws = websocket.create_connection(WS_SUBSCRIBE_URL)
 logging.info("Connection established!")
 ws.send(json.dumps({"op": "blocks_sub"}))
 logging.info("Sending data...")
 
-blocks_timestamps = collections.deque(maxlen=BLOCKS_FOR_MTP)
 
 while True:
     logging.info("Waiting for a block...")
+    print(blocks_timestamps)
     result = json.loads(ws.recv())
     logging.info("Received block with hash %s. Calculating...", result['x']['hash'])
-    blocks_timestamps.append(result['x']['time'])
-    if len(blocks_timestamps) == BLOCKS_FOR_MTP and statistics.median(blocks_timestamps) >= hardfork_timestamp:
+
+    blocks_timestamps.appendleft(result['x']['time'])  # append left coz it's sorted from the newest to the oldest
+
+    if statistics.median(blocks_timestamps) >= hardfork_timestamp:
         logging.info("The HardFork has come! Beware...")
         ws.close()
-        break
+        raise SystemExit("Thank you for using our program")
